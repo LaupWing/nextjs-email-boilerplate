f2715b5541fe02ad581b1883ba8b13ec22e4333d
laupwing <loc_strict@hotmail.com>
2024-04-04 13:23:53 +0200

    Add resizable navigation panel to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import { ResizablePanelGroup } from "@/components/ui/resizable"
+import { ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable"
 import { TooltipProvider } from "@/components/ui/tooltip"
+import { useState } from "react"
 
 export default function Home() {
+    const [isCollapsed, setIsCollapsed] = useState(false)
+    const defaultLayout = [265, 440, 655]
+    const navCollapsedSize = 4
     return (
         <TooltipProvider delayDuration={0}>
             <ResizablePanelGroup
@@ -12,7 +16,25 @@ export default function Home() {
                     )}`
                 }}
                 className="h-full max-h-[800px] items-stretch"
-            ></ResizablePanelGroup>
+            >
+                <ResizablePanel
+                    defaultSize={defaultLayout[0]}
+                    collapsedSize={navCollapsedSize}
+                    collapsible={true}
+                    minSize={15}
+                    maxSize={20}
+                    onCollapse={(collapsed) => {
+                        setIsCollapsed(collapsed)
+                        document.cookie = `react-resizable-panels:collapsed=${JSON.stringify(
+                            collapsed
+                        )}`
+                    }}
+                    className={cn(
+                        isCollapsed &&
+                            "min-w-[50px] transition-all duration-300 ease-in-out"
+                    )}
+                ></ResizablePane>
+            </ResizablePanelGroup>
         </TooltipProvider>
     )
 }
