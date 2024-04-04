1a0a6fed217a2a3537a121581471651fa6824657
laupwing <loc_strict@hotmail.com>
2024-04-04 13:21:21 +0200

    Add ResizablePanelGroup component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import { ResizablePanelGroup } from "@/components/ui/resizable"
 import { TooltipProvider } from "@/components/ui/tooltip"
 
 export default function Home() {
     return (
         <TooltipProvider delayDuration={0}>
-            <div></div>
+            <ResizablePanelGroup
+                direction="horizontal"
+                onLayout={(sizes: number[]) => {
+                    document.cookie = `react-resizable-panels:layout=${JSON.stringify(
+                        sizes
+                    )}`
+                }}
+                className="h-full max-h-[800px] items-stretch"
+            ></ResizablePanelGroup>
         </TooltipProvider>
     )
 }
